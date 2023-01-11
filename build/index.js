5303b2ede72f2136d1ef337ee8484ab17fc0b32b
lcd97 <danycordero9@gmail.com>
2023-01-11 13:22:02 -0600

    Introducción y configuración de Typescript

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Trabajando con Typescript y Nodemon");
+//LOS COMENTARIOS SON IGUALES AL PARECER
+/*
+    ES LO MISMO X2
+*/ 
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log("Trabajando con Typescript y Nodemon")
+
+//LOS COMENTARIOS SON IGUALES AL PARECER
+
+/*
+    ES LO MISMO X2
+*/
